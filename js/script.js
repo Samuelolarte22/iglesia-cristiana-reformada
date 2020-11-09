531d0e5533b1932c89ad49d613eeb5099abcf954
Samuelolarte22 <samuelolarte22@gmail.com>
2020-11-09 11:39:06 -0500

    version xdxdxd

--- js/script.js
+++ js/script.js
@@ -48,7 +48,7 @@ window.onscroll = function(){
 document.getElementById("icon-search").addEventListener("click", mostrar_buscador);
 document.getElementById("cover-ctn-search").addEventListener("click", ocultar_buscador);
 
-//Declarando variables
+// variables
 bars_search =         document.getElementById("ctn-bars-search");
 cover_ctn_search =    document.getElementById("cover-ctn-search");
 inputSearch =         document.getElementById("inputSearch");
